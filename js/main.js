b70b40b299f94629a50d6946b5d5b8e3eafac52a
Viktor Kotlov <kviktors1985@gmail.com>
2022-03-06 09:00:47 +0400

    reinstall eslint with npm

--- js/main.js
+++ js/main.js
@@ -1,18 +1,31 @@
 // Использовал источник:
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// function getRandomInt(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  if (min >= 0 && max > min) {
-    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-  }
-  return 'Введите положительное значение.';
+//   if (min >= 0 && max > min) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+//   }
+
+//   return 'Введите положительное значение.';
+// }
+
+// Функция взята из интернета и доработана
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
 }
+
 
 function getCheckMaxStringLength(stringTest, maxLength) {
   return stringTest.length <= maxLength;
 }
 
-getRandomInt(1, 10);
+getRandomPositiveInteger(1, 10);
 getCheckMaxStringLength('', 50);
