cd05d7d985e8b52f2ff30abce2591fe668df4e11
Viktor Kotlov <kviktors1985@gmail.com>
2022-03-24 15:13:29 +0400

    исправляю замечания -2

--- js/create-miniature.js
+++ js/create-miniature.js
@@ -1,23 +1,23 @@
 import {buildDescriptionUserFoto} from './data.js';
 
 const templeatePicture = document.querySelector('#picture').content; // Находим фрагмент с содержимым темплейта
-const templeateMiniature = templeatePicture.querySelector('.picture');
+const templateMiniature = templeatePicture.querySelector('.picture');
 
-const userImages = document.querySelector('.pictures');
+const userImage = document.querySelector('.pictures');
 
-const templeateFragment = document.createDocumentFragment();
+const templateFragment = document.createDocumentFragment();
 
-const Miniature = () => {
+const createMiniature = () => {
   buildDescriptionUserFoto().forEach(({url, likes, comments}) => {
-    const miniatureElement = templeateMiniature.cloneNode(true);
+    const miniatureElement = templateMiniature.cloneNode(true);
 
     miniatureElement.querySelector('.picture__img').src = url;
     miniatureElement.querySelector('.picture__likes').textContent = likes;
     miniatureElement.querySelector('.picture__comments').textContent = comments.length;
 
-    templeateFragment.appendChild(miniatureElement);
+    templateFragment.appendChild(miniatureElement);
   });
-  userImages.appendChild(templeateFragment);
+  userImage.appendChild(templateFragment);
 };
 
-export {Miniature};
+export {createMiniature};
