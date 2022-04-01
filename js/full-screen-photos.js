48bb4d2b0cc7226391a0423ce9d7d9ceb5e92401
Viktor Kotlov <kviktors1985@gmail.com>
2022-04-01 19:54:29 +0400

    исправил замечания

---
+++ js/full-screen-photos.js
@@ -0,0 +1,54 @@
+import {onBigPhotoEscKey} from './render-photos.js';
+
+const photoPopup = document.querySelector('.big-picture');
+const socialCommentCount = photoPopup.querySelector('.social__comment-count');
+const commentsLoader = photoPopup.querySelector('.comments-loader');
+const templateComment = document.querySelector('#comment').content.querySelector('.social__comment');
+const containerComments = document.querySelector('.social__comments');
+const elementFragment = document.createDocumentFragment();
+
+
+const closeBigPhoto = () => {
+  photoPopup.classList.add('hidden');
+  socialCommentCount.classList.remove('hidden');
+  commentsLoader.classList.remove('hidden');
+  document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onBigPhotoEscKey);
+  containerComments.querySelectorAll('.social__comment').forEach((item) => item.remove());
+};
+
+const openBigPhoto = () => {
+  photoPopup.classList.remove('hidden');
+  socialCommentCount.classList.add('hidden');
+  commentsLoader.classList.add('hidden');
+  document.body.classList.add('modal-open');
+  document.addEventListener('keydown', onBigPhotoEscKey);
+};
+
+const onBigPhotoClick = () => {
+  closeBigPhoto();
+};
+
+photoPopup.querySelector('#picture-cancel').addEventListener('click', onBigPhotoClick);
+
+const renderComment = (item) => {
+  const comment = templateComment.cloneNode(true);
+  comment.querySelector('.social__picture').setAttribute('src', item.avatar);
+  comment.querySelector('.social__picture').setAttribute('alt', item.name);
+  comment.querySelector('.social__text').textContent = item.message;
+  return comment;
+};
+
+const renderBigPhoto = (item) => {
+  photoPopup.querySelector('.big-picture__img>img').setAttribute('src', item.url);
+  photoPopup.querySelector('.likes-count').textContent = item.likes;
+  photoPopup.querySelector('.comments-count').textContent = item.comments.length;
+  photoPopup.querySelector('.social__caption').textContent = item.description;
+  item.comments.forEach((comment) => {
+    const commentLast = renderComment(comment);
+    elementFragment.append(commentLast);
+  });
+  containerComments.append(elementFragment);
+};
+
+export {renderBigPhoto, openBigPhoto, closeBigPhoto};
--- js/render-photos.js
+++ js/render-photos.js
@@ -1,23 +1,35 @@
-import { renderBigPhoto } from './full-screen-fotos.js';
-const template = document.querySelector('#picture').content.querySelector('.picture');
-const container = document.querySelector('.pictures');
+import {isEscEvent} from './util.js';
+import {renderBigPhoto, openBigPhoto, closeBigPhoto} from './full-screen-photos.js';
+
+const templatePicture = document.querySelector('#picture').content.querySelector('.picture');
+const containerUsersPictures = document.querySelector('.pictures');
 const elementFragment = document.createDocumentFragment();
+
 const renderThumbnail = (item) => {
-  const thumbnail = template.cloneNode(true);
+  const thumbnail = templatePicture.cloneNode(true);
   thumbnail.querySelector('.picture__img').setAttribute('src', item.url);
   thumbnail.querySelector('.picture__likes').textContent = item.likes;
   thumbnail.querySelector('.picture__comments').textContent = item.comments.length;
   return thumbnail;
 };
+
+const onBigPhotoEscKey = (evt) => {
+  if (isEscEvent(evt)) {
+    evt.preventDefault();
+    closeBigPhoto();
+  }
+};
+
 const renderPhotos = (items) => {
   items.forEach((item) => {
     const thumbnail = renderThumbnail(item);
     thumbnail.addEventListener('click', () => {
       renderBigPhoto(item);
+      openBigPhoto();
     });
     elementFragment.append(thumbnail);
   });
-  return container.append(elementFragment);
+  return containerUsersPictures.append(elementFragment);
 };
 
-export {renderPhotos};
+export {renderPhotos, onBigPhotoEscKey};
